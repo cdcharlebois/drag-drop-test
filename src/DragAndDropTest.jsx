17c1f4b8b71846b1da9683f6ec54f37675cc68e1
Conner Charlebois <cdcharlebois@gmail.com>
2020-08-25 14:36:16 -0400

    state to track open nodes

--- src/DragAndDropTest.jsx
+++ src/DragAndDropTest.jsx
@@ -1,4 +1,4 @@
-import { Component, createElement } from "react";
+import { Component, createElement, useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { hot } from "react-hot-loader/root";
@@ -7,9 +7,27 @@ import "./ui/DragAndDropTest.css";
 import { OrgChart } from "./components/OrgChart";
 
 const DragAndDropTest = props => {
-    const { onDragEnd, onDragStart, content } = props;
-    const { parent, childkey: key } = props;
-    const { datasource: ds } = props;
+    const { onDragEnd, onDragStart, content, parent, childkey: key, datasource: ds } = props;
+    const [openNodes, setOpenNodes] = useState([]);
+    /**
+     * toggles an item open/closed (so that its children are shown or hidden)
+     * @param {ds item} item - the item to toggle open/closed
+     */
+    const toggleOpen = item => {
+        const isOpen = openNodes.find(openNode => {
+            return openNode.id === item.id;
+        });
+        if (isOpen) {
+            // close
+            setOpenNodes(
+                openNodes.filter(openNode => {
+                    return openNode.id != item.id;
+                })
+            );
+        } else {
+            setOpenNodes([...openNodes, item]);
+        }
+    };
     /**
      * removes any found items from the universe so they are not checked again.
      * @param {[ds item]} targets - the ds items that we want to remove from the unviverse
@@ -63,7 +81,14 @@ const DragAndDropTest = props => {
     };
     return (
         <DndProvider backend={HTML5Backend}>
-            <OrgChart data={_getDataMap(ds.items)} onDrop={onDragEnd} onDrag={onDragStart} node={content} />
+            <OrgChart
+                data={_getDataMap(ds.items)}
+                onDrop={onDragEnd}
+                onDrag={onDragStart}
+                node={content}
+                openNodes={openNodes}
+                toggleOpen={toggleOpen}
+            />
         </DndProvider>
     );
 };
---
+++ src/components/Node.jsx
@@ -0,0 +1,35 @@
+import { Component, createElement, useState } from "react";
+import { Tree, TreeNode } from "react-organizational-chart";
+import { DraggableItem } from "./DraggableItem";
+
+export const Node = props => {
+    const { onDrag, onDrop, node, item, toggleOpen, isOpen } = props;
+    const _renderChildrenTreeNodes = children => {
+        return children.map(item => (
+            <Node onDrag={onDrag} onDrop={onDrop} node={node} item={item} toggleOpen={toggleOpen} isOpen={isOpen} />
+        ));
+    };
+    const hasChildren = item.children && item.children.length > 0;
+    return hasChildren ? (
+        <TreeNode
+            label={
+                <div>
+                    <DraggableItem onDrop={onDrop(item)} onDrag={onDrag(item)}>
+                        {node(item)}
+                    </DraggableItem>
+                    <div onClick={() => toggleOpen(item)}>{isOpen(item) ? `-` : `+`}</div>
+                </div>
+            }
+        >
+            {isOpen(item) ? _renderChildrenTreeNodes(item.children) : null}
+        </TreeNode>
+    ) : (
+        <TreeNode
+            label={
+                <DraggableItem onDrop={onDrop(item)} onDrag={onDrag(item)}>
+                    {node(item)}
+                </DraggableItem>
+            }
+        />
+    );
+};
